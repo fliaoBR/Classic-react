9d8e61de1744b4e08380f8f32175e33727ef0096
fliaoBR <fliao@bluroot.ca>
2023-01-18 17:33:12 -0500

    stuf

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	entry: "./App/index.jsx",
+	entry: "./app/index.jsx",
 	output: {
 		path: path.resolve(__dirname, "dist"),
 		filename: "index_bundle.js",
@@ -17,7 +17,7 @@ module.exports = {
 	resolve: {
 		extensions: [".jsx", "..."],
 	},
-	mode: process.env.NODE_ENV == "production" ? "production" : "development",
+	mode: process.env.NODE_ENV === "production" ? "production" : "development",
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: "app/index.html",
